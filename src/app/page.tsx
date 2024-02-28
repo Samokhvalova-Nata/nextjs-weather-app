46136d86d1c36b8aa0c0b81a3469c6a83bf17fc4
Nataliia Samokhvalova <samohvalova520@gmail.com>
2024-02-28 17:02:30 +0200

    Add API_KEY and data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,76 @@
 import Navbar from "@/components/Navbar";
+import { useQuery } from "react-query";
+
+interface WeatherData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherListItem[];
+  city: CityInfo;
+}
+
+interface WeatherListItem {
+  dt: number;
+  main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      sea_level: number;
+      grnd_level: number;
+      humidity: number;
+      temp_kf: number;
+  };
+  weather: WeatherInfo[];
+  clouds: {
+      all: number;
+  };
+  wind: {
+      speed: number;
+      deg: number;
+      gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+      pod: string;
+  };
+  dt_txt: string;
+}
+
+interface WeatherInfo {
+  id: number;
+  main: string;
+  description: string;
+  icon: string;
+}
+
+interface CityInfo {
+  id: number;
+  name: string;
+  coord: {
+      lat: number;
+      lon: number;
+  };
+  country: string;
+  population: number;
+  timezone: number;
+  sunrise: number;
+  sunset: number;
+}
+
 
 export default function Home() {
+  const { isLoading, error, data } = useQuery('repoData', async () =>
+  fetch('https://api.openweathermap.org/data/2.5/forecast?q=kharkiv&appid=dbbd91c36a36889b12bfe4739683093b&cnt=56').then(res =>
+    res.json()
+  )
+)
+
+if (isLoading) return 'Loading...'
+
+
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar/>
