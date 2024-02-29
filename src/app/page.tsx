3597ffbe559109f94f73de53369dad929721ac58
Nataliia Samokhvalova <samohvalova520@gmail.com>
2024-02-29 16:39:37 +0200

    Add Container component and Today data started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import Container from "@/components/Container";
 import Navbar from "@/components/Navbar";
+import { convertKelvinToCelsius } from "@/utils/convertKelvinToCelsius";
+import axios from "axios";
+import { format, parseISO } from "date-fns";
 import { useQuery } from "react-query";
 
 interface WeatherData {
@@ -7,7 +13,7 @@ interface WeatherData {
   cnt: number;
   list: WeatherListItem[];
   city: CityInfo;
-}
+};
 
 interface WeatherListItem {
   dt: number;
@@ -37,14 +43,14 @@ interface WeatherListItem {
       pod: string;
   };
   dt_txt: string;
-}
+};
 
 interface WeatherInfo {
   id: number;
   main: string;
   description: string;
   icon: string;
-}
+};
 
 interface CityInfo {
   id: number;
@@ -58,22 +64,63 @@ interface CityInfo {
   timezone: number;
   sunrise: number;
   sunset: number;
-}
+};
 
 
 export default function Home() {
-  const { isLoading, error, data } = useQuery('repoData', async () =>
-  fetch('https://api.openweathermap.org/data/2.5/forecast?q=kharkiv&appid=dbbd91c36a36889b12bfe4739683093b&cnt=56').then(res =>
-    res.json()
-  )
-)
+  const { isLoading, error, data } = useQuery<WeatherData>(
+    'repoData', 
+    async () => {
+      const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=kharkiv&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56`);
 
-if (isLoading) return 'Loading...'
+      return data;
+  }
+);
 
+const firstData = data?.list[0];
+// console.log('data', data?.city);
+
+if (isLoading) 
+  return (
+  <div className="flex items-center min-h-screen justify-center">
+    <p className="animate-bounce">Loading...</p>
+  </div>
+);
 
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
       <Navbar/>
+      <main className="px-3 max-w-7xl mx-auto flex flex-col gap-9 w-full pb-10 pt-4">
+        {/* {today data} */}
+        <section className="space-y-4">
+          <div className="space-y-2">
+            <h2 className="flex gap-1 text-2xl items-end">
+              <p>{format(parseISO(firstData?.dt_txt ?? ''), 'EEEE')}</p>
+              <p className="text-lg">({format(parseISO(firstData?.dt_txt ?? ''), 'dd.MM.yyyy')})</p>
+            </h2>
+            <Container className="gap-10 px-6 items-center">
+              <div className="flex flex-col px-4">
+                <span className="text-5xl">
+                  {convertKelvinToCelsius(firstData?.main.temp ?? 0)}°
+                </span>
+                <p className="text-xs space-x-1 whitespace-nowrap">
+                  <span>Feels like</span>
+                  <span>{convertKelvinToCelsius(firstData?.main.feels_like ?? 0)}°</span>
+                </p>
+                <p className="text-xs space-x-2">
+                  <span>{convertKelvinToCelsius(firstData?.main.temp_min ?? 0)}° ↓{" "}</span>
+                  <span>{" "}{convertKelvinToCelsius(firstData?.main.temp_max ?? 0)}°↑ </span>
+
+                </p>
+              </div>
+            </Container>
+          </div>
+        </section>
+
+        {/* {7 day forcast data} */}
+        <section></section>
+
+      </main>
     </div>
   ); 
 }
