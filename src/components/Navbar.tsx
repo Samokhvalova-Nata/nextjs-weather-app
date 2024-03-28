32d29abaa8abef6aba0cae3fdf83a085cebfe669
Nataliia Samokhvalova <samohvalova520@gmail.com>
2024-03-28 14:22:07 +0200

    Add getting Current Location

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,6 +8,7 @@ import { useAtom } from 'jotai';
 import { loadingCityAtom, placeAtom } from '@/app/atom';
 
 type Props = { location?: string };
+const API_KEY = process.env.NEXT_PUBLIC_WEATHER_KEY;
 
 export default function Navbar({location}: Props) {
     const [city, setCity] = useState('');
@@ -59,8 +60,27 @@ export default function Navbar({location}: Props) {
         }
     };
 
+    function handleCurrentLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(async (position) => {
+                const { latitude, longitude } = position.coords;
+                try {
+                    setLoadingCity(true);
+                    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`);
+                    setTimeout(() => {
+                        setLoadingCity(false);
+                        setPlace(response.data.name);
+                    }, 500);
+                } catch (error) {
+                    setLoadingCity(false);
+                }
+            })
+        }
+    }
+
     return (
-        <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
+        <>
+            <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
             <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
                 <div className='flex items-center justify-center gap-2'>
                     <h2 className='text-gray-500 text-3xl'>Weather</h2>
@@ -68,11 +88,14 @@ export default function Navbar({location}: Props) {
                 </div>
                 { /* */}
                 <section className='flex gap-2 items-center'>
-                    <MdMyLocation className='text-2xl text-gray-400 hover:opacity-80'/>
+                    <MdMyLocation
+                        title='Your Current Location'
+                        onClick={handleCurrentLocation}
+                        className='text-2xl text-gray-400 hover:opacity-80' />
                     <MdOutlineLocationOn className='text-3xl'/>
                     <p className='text-slate-900/80 text-sm'>{location}</p>
                     {/* SearchBox */}
-                    <div className='relative'>
+                    <div className='relative hidden md:flex'>
                         <SearchBox value={city} 
                         onSubmit={handleSubmitSearch} 
                         onChange={(event) => handleInputChange(event.target.value)}/>
@@ -84,7 +107,21 @@ export default function Navbar({location}: Props) {
                     </div>
                 </section>
             </div>
-        </nav>
+            </nav>
+            <section className='flex max-w-7xl px-3 md:hidden'>
+                <div className='relative'>
+                        <SearchBox value={city} 
+                        onSubmit={handleSubmitSearch} 
+                        onChange={(event) => handleInputChange(event.target.value)}/>
+                        <SuggestionBox 
+                            {...{showSuggestions, 
+                                suggestions, 
+                                handleSuggestionClick, 
+                                error}}/>
+                </div>
+            </section>
+        </>
+
     );
 };
 
